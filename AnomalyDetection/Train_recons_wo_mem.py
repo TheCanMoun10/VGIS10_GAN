47dad3190af817e1fce6fa226e57080a34741c79
Andreas <andreas24daasbjerg@gmail.com>
2023-03-30 10:53:23 +0200

    Added MNAD files and updated .gitignore

---
+++ AnomalyDetection/Train_recons_wo_mem.py
@@ -0,0 +1,210 @@
+import numpy as np
+import os
+import sys
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+import torch.nn.init as init
+import torch.utils.data as data
+import torch.utils.data.dataset as dataset
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+import torchvision.utils as v_utils
+import matplotlib.pyplot as plt
+import cv2
+import math
+from collections import OrderedDict
+import copy
+import time
+from model.utils import DataLoader
+from model.reconstruction_wo_memory import *
+from sklearn.metrics import roc_auc_score
+from utils import *
+import random
+import wandb
+import argparse
+import datetime
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+parser = argparse.ArgumentParser(description="MNAD")
+parser.add_argument('--gpus', nargs='+', type=str, help='gpus')
+parser.add_argument('--model', type=str, default="convAE", help='specify model architecture. default: convAE')
+parser.add_argument('--batch_size', type=int, default=4, help='batch size for training')
+parser.add_argument('--test_batch_size', type=int, default=1, help='batch size for test')
+parser.add_argument('--epochs', type=int, default=60, help='number of epochs for training')
+parser.add_argument('--loss_compact', type=float, default=0.01, help='weight of the feature compactness loss')
+parser.add_argument('--loss_separate', type=float, default=0.01, help='weight of the feature separateness loss')
+parser.add_argument('--h', type=int, default=256, help='height of input images')
+parser.add_argument('--w', type=int, default=256, help='width of input images')
+parser.add_argument('--c', type=int, default=3, help='channel of input images')
+parser.add_argument('--lr', type=float, default=2e-5, help='initial learning rate')
+parser.add_argument('--t_length', type=int, default=2, help='length of the frame sequences')
+parser.add_argument('--fdim', type=int, default=512, help='channel dimension of the features')
+parser.add_argument('--mdim', type=int, default=512, help='channel dimension of the memory items')
+parser.add_argument('--msize', type=int, default=10, help='number of the memory items')
+parser.add_argument('--alpha', type=float, default=0.6, help='weight for the anomality score')
+parser.add_argument('--num_workers', type=int, default=2, help='number of workers for the train loader')
+parser.add_argument('--num_workers_test', type=int, default=1, help='number of workers for the test loader')
+parser.add_argument('--dataset_type', type=str, default='ped2', help='type of dataset: ped2, avenue, shanghai')
+parser.add_argument('--dataset_path', type=str, default='./datasets/', help='directory of data')
+parser.add_argument('--exp_dir', type=str, default='log', help='directory of log')
+parser.add_argument('--nega_loss', type=bool, default=False, help='Apply negative loss to model')
+parser.add_argument('--nega_value', type=float, default=0.02, help='Value to degrade loss')
+parser.add_argument('--mode', type=str, default="train")
+
+args = parser.parse_args()
+
+wandb.init(project="VGIS10_MNAD",
+           
+           config={
+               "learning_rate": args.lr,
+               "architecture" : args.model,
+               "dataset": args.dataset_type,
+               "epochs" : args.epochs,
+               "batch size" : args.batch_size,
+               "negative loss" : args.nega_loss,
+               "mode" : args.mode,
+               },
+            name=f'{args.mode}_{args.dataset_type}_batch{args.batch_size}_epochs{args.epochs}_lr{args.lr}'
+           
+           )
+
+torch.manual_seed(2020)
+
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+if args.gpus is None:
+    gpus = "0"
+    os.environ["CUDA_VISIBLE_DEVICES"]= gpus
+else:
+    gpus = ""
+    for i in range(len(args.gpus)):
+        gpus = gpus + args.gpus[i] + ","
+    os.environ["CUDA_VISIBLE_DEVICES"]= gpus[:-1]
+
+torch.backends.cudnn.enabled = True # make sure to use cudnn for computational performance
+
+train_folder = args.dataset_path+args.dataset_type+"/training/frames"
+test_folder = args.dataset_path+args.dataset_type+"/testing/frames"
+
+# Loading dataset
+train_dataset = DataLoader(train_folder, transforms.Compose([
+             transforms.ToTensor(),          
+             ]), resize_height=args.h, resize_width=args.w, time_step=args.t_length-1,num_pred = 0)
+
+test_dataset = DataLoader(test_folder, transforms.Compose([
+             transforms.ToTensor(),            
+             ]), resize_height=args.h, resize_width=args.w, time_step=args.t_length-1,num_pred = 0)
+
+train_size = len(train_dataset)
+test_size = len(test_dataset)
+
+train_batch = data.DataLoader(train_dataset, batch_size = args.batch_size, 
+                              shuffle=True, num_workers=args.num_workers, drop_last=True)
+test_batch = data.DataLoader(test_dataset, batch_size = args.test_batch_size, 
+                             shuffle=False, num_workers=args.num_workers_test, drop_last=False)
+
+
+# Model setting
+
+model = convAE(args.c, args.t_length, args.msize, args.fdim, args.mdim)
+
+params_encoder =  list(model.encoder.parameters()) 
+params_decoder = list(model.decoder.parameters())
+params = params_encoder + params_decoder
+optimizer = torch.optim.Adam(params, lr = args.lr)
+scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer,T_max =args.epochs)
+model.cuda()
+wandb.watch(model)
+
+# Report the training process
+log_dir = os.path.join('./exp', args.dataset_type, f'{args.exp_dir}_lr{args.lr}')
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+
+loss_func_mse = nn.MSELoss(reduction='none')
+
+# Training
+best_test = float("inf")
+#m_items = F.normalize(torch.rand((args.msize, args.mdim), dtype=torch.float), dim=1).cuda() # Initialize the memory items
+
+
+for epoch in range(args.epochs):
+    labels_list = []
+    example_images = []
+    model.train()
+    start = time.time()
+    train_loss = AverageMeter()
+    for j,(imgs) in enumerate(train_batch):
+        
+        imgs = Variable(imgs).cuda()
+        outputs = model.forward(imgs, True)
+        
+        # Visualize images on wandb:
+        pixels = outputs[0].detach().cpu().permute(1,2,0).numpy()
+        np.rot90(pixels, k=0, axes=(1,0))
+        
+        optimizer.zero_grad()
+        loss_pixel = torch.mean(loss_func_mse(outputs, imgs))
+        loss = loss_pixel
+        # loss = loss_pixel + args.loss_compact * compactness_loss + args.loss_separate * separateness_loss
+        train_loss.update(loss.item(),imgs.size(0))
+        if args.nega_loss == True:
+            loss = -args.nega_value*loss 
+        loss.backward(retain_graph=True)
+        optimizer.step()
+        
+        if(j%250 == 0):
+            image = wandb.Image(pixels, caption=f"Generated anomaly_{j}_epoch{epoch+1}")
+            example_images.append(image)
+            print(
+                f'Epoch [{epoch+1}/{args.epochs}]\t'
+                f'Step [{j+1}/{len(train_batch)}]\t'
+                f'Training Loss: {loss.item():.4f}'
+                )
+            wandb.log({'examples': example_images})
+        
+        wandb.log({'Train_loss_avg':train_loss.avg ,'Loss': loss})
+            
+    if(epoch%5 == 0):
+        if epoch == 0:
+            torch.save(model, os.path.join(log_dir, f'{epoch}_model.pth'))
+        else:
+            torch.save(model, os.path.join(log_dir, f'{epoch+1}_model.pth'))
+        #torch.save(m_items, os.path.join(log_dir, f'{epoch}_m_items.pt')) 
+    scheduler.step()
+    
+    print('----------------------------------------')
+    print('Epoch:', epoch+1)
+    print('Loss: Reconstruction {:.6f}'.format(loss_pixel.item()))
+    print('----------------------------------------')
+    print(f'Train loss avg: {train_loss.avg}')
+    
+today = datetime.datetime.today()
+timestring = f"{today.date}-{today.hour}:{today.minute}"
+torch.save(model, os.path.join(log_dir, timestring, f'{epoch+1}_model.pth'))
+#torch.save(m_items, os.path.join(log_dir, f'{epoch}_m_items.pt')) 
+print('Training is finished')
+print(log_dir)
+
+# Save the model and the memory items
+
+
--- AnomalyDetection/train.py
+++ AnomalyDetection/train.py
@@ -1,66 +1,178 @@
-# import numpy as np
-# from datetime import datetime
-
+import numpy as np
+import os
+import sys
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
+import torch.optim as optim
+import torchvision
+import torch.nn.init as init
+import torch.utils.data as data
+import torch.utils.data.dataset as dataset
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+import torchvision.utils as v_utils
 import matplotlib.pyplot as plt
+import cv2
+import math
+from collections import OrderedDict
+import copy
+import time
+from model.utils import DataLoader
+from sklearn.metrics import roc_auc_score
+from utils import *
+import random
 
-# Import helper functions and network architecture:
-from helper_functions import *
-from network import LeNet5
+import argparse
 
-# TODO: Add Avenue dataset loader to train.py and UNet architecture to network.py.
 
-# Check if device is available:
+parser = argparse.ArgumentParser(description="MNAD")
+parser.add_argument('--gpus', nargs='+', type=str, help='gpus')
+parser.add_argument('--batch_size', type=int, default=4, help='batch size for training')
+parser.add_argument('--test_batch_size', type=int, default=1, help='batch size for test')
+parser.add_argument('--epochs', type=int, default=60, help='number of epochs for training')
+parser.add_argument('--loss_compact', type=float, default=0.1, help='weight of the feature compactness loss')
+parser.add_argument('--loss_separate', type=float, default=0.1, help='weight of the feature separateness loss')
+parser.add_argument('--h', type=int, default=256, help='height of input images')
+parser.add_argument('--w', type=int, default=256, help='width of input images')
+parser.add_argument('--c', type=int, default=3, help='channel of input images')
+parser.add_argument('--lr', type=float, default=2e-4, help='initial learning rate')
+parser.add_argument('--method', type=str, default='pred', help='The target task for anoamly detection')
+parser.add_argument('--t_length', type=int, default=5, help='length of the frame sequences')
+parser.add_argument('--fdim', type=int, default=512, help='channel dimension of the features')
+parser.add_argument('--mdim', type=int, default=512, help='channel dimension of the memory items')
+parser.add_argument('--msize', type=int, default=10, help='number of the memory items')
+parser.add_argument('--num_workers', type=int, default=2, help='number of workers for the train loader')
+parser.add_argument('--num_workers_test', type=int, default=1, help='number of workers for the test loader')
+parser.add_argument('--dataset_type', type=str, default='avenue', help='type of dataset: ped2, avenue, shanghai')
+parser.add_argument('--dataset_path', type=str, default='./datasets', help='directory of data')
+parser.add_argument('--exp_dir', type=str, default='log', help='directory of log')
+parser.add_argument('--neg_loss', type=float, default=0.2, help='Value to degrade loss during training.')
+
+args = parser.parse_args()
+
+# os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+# if args.gpus is None:
+#     gpus = "0"
+#     os.environ["CUDA_VISIBLE_DEVICES"]= gpus
+# else:
+#     gpus = ""
+#     for i in range(len(args.gpus)):
+#         gpus = gpus + args.gpus[i] + ","
+#     os.environ["CUDA_VISIBLE_DEVICES"]= gpus[:-1]
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 device = torch.device(DEVICE)
+torch.backends.cudnn.enabled = True # make sure to use cudnn for computational performance
 
-# Parameters for the network:
-RANDOM_SEED = 42
-LEARNING_RATE = 0.0001
-BATCH_SIZE = 32
-NUM_EPOCHS = 25
+train_folder = args.dataset_path+"/"+args.dataset_type+"/training/frames"
+test_folder = args.dataset_path+"/"+args.dataset_type+"/testing/frames"
 
-NUM_CHANNELS = 1
-NUM_CLASSES = 1
-NUM_LEVELS = 3
+# Loading dataset
+train_dataset = DataLoader(train_folder, transforms.Compose([
+             transforms.ToTensor(),          
+             ]), resize_height=args.h, resize_width=args.w, time_step=args.t_length-1)
 
-IMG_SIZE = 32
-N_CLASSES = 10
+test_dataset = DataLoader(test_folder, transforms.Compose([
+             transforms.ToTensor(),            
+             ]), resize_height=args.h, resize_width=args.w, time_step=args.t_length-1)
+
+train_size = len(train_dataset)
+print(f"Train size: {train_size}")
+test_size = len(test_dataset)
+print(f"Train  size: {train_size}")
+
+train_batch = data.DataLoader(train_dataset, batch_size = args.batch_size, 
+                              shuffle=True, num_workers=args.num_workers, drop_last=True)
+test_batch = data.DataLoader(test_dataset, batch_size = args.test_batch_size, 
+                             shuffle=False, num_workers=args.num_workers_test, drop_last=False)
 
 
-# define transforms
-transforms = transforms.Compose([
-                                 transforms.Resize((32,32)),
-                                 transforms.ToTensor(),
-                                 transforms.Normalize(mean = (0.1325,), std = (0.3105,))
-                                ])
+# Model setting
+assert args.method == 'pred' or args.method == 'recon', 'Wrong task name'
+if args.method == 'pred':
+    from model.final_future_prediction_with_memory_spatial_sumonly_weight_ranking_top1 import *
+    model = convAE(args.c, args.t_length, args.msize, args.fdim, args.mdim)
+else:
+    from model.Reconstruction import *
+    model = convAE(args.c, memory_size = args.msize, feature_dim = args.fdim, key_dim = args.mdim)
+params_encoder =  list(model.encoder.parameters()) 
+params_decoder = list(model.decoder.parameters())
+params = params_encoder + params_decoder
+optimizer = torch.optim.Adam(params, lr = args.lr)
+scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer,T_max =args.epochs)
+model.to(device)
 
-# Download and create dataset:
-train_dataset = datasets.MNIST(
-                               root='./AnomalyDetection/datasets',
-                               train = True,
-                               transform = transforms,
-                               download = True
-                            )
+
+# Report the training process
+log_dir = os.path.join('./exp', args.dataset_type, args.method, args.exp_dir)
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+orig_stdout = sys.stdout
+f = open(os.path.join(log_dir, f'loss{args.neg_loss}_log.txt'),'w')
+sys.stdout= f
+
+loss_func_mse = nn.MSELoss(reduction='none')
+
+# Training
+
+m_items = F.normalize(torch.rand((args.msize, args.mdim), dtype=torch.float), dim=1).to(device) # Initialize the memory items
+
+for epoch in range(args.epochs):
+    labels_list = []
+    model.train()
+    
+    start = time.time()
+    
+    for j,(imgs) in enumerate(train_batch):
         
-test_dataset = datasets.MNIST(
-                               root='./AnomalyDetection/datasets',
-                               train = False,
-                               transform = transforms
-                            )
+        imgs = Variable(imgs).to(device)
+        
+        if args.method == 'pred':
+            outputs, _, _, m_items, softmax_score_query, softmax_score_memory, separateness_loss, compactness_loss = model.forward(imgs[:,0:12], m_items, True)
+        
+        else:
+            outputs, _, _, m_items, softmax_score_query, softmax_score_memory, separateness_loss, compactness_loss = model.forward(imgs, m_items, True)
+        
+        
+        optimizer.zero_grad()
+        if args.method == 'pred':
+            loss_pixel = torch.mean(loss_func_mse(outputs, imgs[:,12:]))
+        else:
+            loss_pixel = torch.mean(loss_func_mse(outputs, imgs))
+            
+        loss = loss_pixel
+        loss = -args.neg_loss*loss
+        loss.backward(retain_graph=True)
+        optimizer.step()
+        
+        if (j+1) % 350 == 0:
+                print( 
+                    f'Epoch [{epoch+1}/{args.epochs}]\t'
+                    f'Step [{j+1}/{train_size}]\t'
+                    f'Training Loss: {loss.item():.4f}'
+                    )
+        #print(f'Step: [{j}]')
+        
+    scheduler.step()
+    
+    print('----------------------------------------')
+    print('Epoch:', epoch+1)
+    if args.method == 'pred':
+        print('Loss: Prediction {:.6f}/ Compactness {:.6f}/ Separateness {:.6f}'.format(loss_pixel.item(), compactness_loss.item(), separateness_loss.item()))
+    else:
+        print('Loss: Reconstruction {:.6f}/ Compactness {:.6f}/ Separateness {:.6f}'.format(loss_pixel.item(), compactness_loss.item(), separateness_loss.item()))
+    print('Memory_items:')
+    print(m_items)
+    print('----------------------------------------')
+    
+print('Training is finished')
+# Save the model and the memory items
+torch.save(model, os.path.join(log_dir, f'loss{args.neg_loss}_model.pth'))
+torch.save(m_items, os.path.join(log_dir, f'loss{args.neg_loss}_keys.pt'))
+    
+sys.stdout = orig_stdout
+f.close()
 
-# Define the Dataloader:
-train_loader = DataLoader(dataset=train_dataset, batch_size=BATCH_SIZE, shuffle=True)
-test_loader = DataLoader(dataset=test_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
-model = LeNet5(N_CLASSES).to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
-cost = nn.CrossEntropyLoss() # TODO: Change this gradient ascent loss (L1 or L2).
 
-if __name__ == '__main__':
-   torch.manual_seed(RANDOM_SEED)
-   model, optimizer, train_loss, model_accuracy  = training_loop(model, cost, optimizer, train_loader, test_loader, NUM_EPOCHS, device)
