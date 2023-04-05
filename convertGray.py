1bb1ba1c7bb027aeaacab8fe72ed9b097a7925d5
Andreas <andreas24daasbjerg@gmail.com>
2023-04-05 17:19:29 +0200

    Added convertGray.py to convert to grayscale.

---
+++ convertGray.py
@@ -0,0 +1,47 @@
+# Grayscale conversion
+import cv2
+import os
+import glob
+
+origin_path = './train/walk/03/'
+destin_path = './gray/train/walk/03/'
+
+test_origin_path = './test/runbag/'
+test_origin_path2 = './test/walk/'
+test_destin_path = './gray/test/runbag/'
+test_destin_path2 = './gray/test/walk/'
+
+for filename in os.listdir(origin_path):
+    file_path = origin_path + filename
+    img = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
+    cv2.imshow('img', img)
+    print(len(img.shape))
+    cv2.waitKey(0)
+    
+    # gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _, tail = os.path.split(file_path) 
+    new_name = os.path.splitext(tail)[0]
+    final_path = destin_path + f"{new_name}.png"
+    status = cv2.imwrite(final_path, img)
+    
+    print('Image written to file-system :', status)
+    
+for filename in os.listdir(test_origin_path):
+    file_path = test_origin_path + filename
+    img = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
+    
+    # gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _, tail = os.path.split(file_path) 
+    new_name = os.path.splitext(tail)[0]
+    final_path = test_destin_path + f"{new_name}.png"
+    status = cv2.imwrite(final_path, img)
+    
+for filename in os.listdir(test_origin_path2):
+    file_path = test_origin_path + filename
+    img = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
+    
+    # gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _, tail = os.path.split(file_path) 
+    new_name = os.path.splitext(tail)[0]
+    final_path = test_destin_path2 + f"{new_name}.png"
+    status = cv2.imwrite(final_path, img)
