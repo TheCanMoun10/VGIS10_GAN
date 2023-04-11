2ef3d7c5b4b743b9c657d90990ac1d83c6f6a582
Andreas Daasbjerg <58854102+TheCanMoun10@users.noreply.github.com>
2023-04-11 11:36:47 +0200

    Update opts_fine_tune_discriminator.py

--- OGNet/opts_fine_tune_discriminator.py
+++ OGNet/opts_fine_tune_discriminator.py
@@ -17,7 +17,7 @@ def parse_opts_ft():
     parser.add_argument('--test_anomaly_threshold', default='0.5', type=float, help='threshold to print anomaly or not')
     parser.add_argument('--drop_last', default=True, type=bool, help='drop the remaining of the batch if the size doesnt match minimum batch size')
     parser.add_argument('--frame_size', default=64, type=int, help='one side size of the square patch to be extracted from each frame')
-    parser.add_argument('--low_epoch', default=0, type=int, help='low epoch i.e. Gold for phase two')
+    parser.add_argument('--low_epoch', default=2, type=int, help='low epoch i.e. Gold for phase two')
     parser.add_argument('--high_epoch', default=5, type=int, help='high epoch i.e. Gn for phase two')
     parser.add_argument('--iterations', default=75, type=int, help='iterations for phase two')
     parser.add_argument('--wandb', action='store_true', help='Use wandb to log and visualize network training')
